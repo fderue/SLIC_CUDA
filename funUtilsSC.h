4a88791bcc85ccde9426fd8aea5fd8a234a31f13
fderue <francois-xavier.derue@hotmail.com>
2016-08-04 01:42:29 -0400

    delete funUtils

--- funUtilsSC.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-
-
-/* determine width and height (integer value) of an initial spx from its expected size d. */
-void getWlHl(int w, int h, int d, int & wl, int & hl);
-inline int iDivUp(int a, int b){ return (a%b==0)? a/b : a/b+1; }
